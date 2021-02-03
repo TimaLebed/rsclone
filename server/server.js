7fabd3cf348bb09259d20fcce2c02b1dc0db88fa
TFX-98 <e.chaika.private@yandex.by>
2021-02-03 21:20:33 +0300

    feat: add search opponent

--- server/server.js
+++ server/server.js
@@ -5,24 +5,23 @@ const DOCROOT = './../dist/';
 const http = require('http');
 const path = require('path');
 const express = require('express');
-const socketIO = require('socket.io');
+const sockets = require('./sockets');
 
 // 2. Создать сервер, используя express и http
 const app = express();
 const server = http.createServer(app);
+
 
 // 3. Настроить отдачу игры при запросе к серверу
 const documentRoot = path.join(__dirname, DOCROOT);
 const staticContent = express.static(documentRoot);
 app.use(staticContent);
 
-// 4. запускаем сервер
+// 4. инициализируем сокеты
+sockets.init(server);
+
+// 5. запускаем сервер
 server.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
 });
 
-const io = socketIO(server);
-io.on('connection', socket => {
-    socket.emit('gameStart');
-    console.log(`new user connected ${socket.id}`);
-});
